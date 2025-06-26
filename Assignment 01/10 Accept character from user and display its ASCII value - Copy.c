95324049344fffaf928821a9c108faea35254568
MoreAtharv <moreatharv78@gmail.com>
2025-06-26 07:52:41 +0530

    Assignment 01

---
+++ Assignment 01/10 Accept character from user and display its ASCII value - Copy.c
@@ -0,0 +1,20 @@
+///
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    char ch;
+
+    printf("Enter Character");
+    scanf("%c",&ch);
+
+    printf("\nCharacter:%c",ch);
+    printf("\nDecimal:%d",ch);
+    printf("\nHexadecimal:%x",ch);
+    printf("\nOctal:%o",ch);
+
+    getch();
+
+    return 0;
+}
