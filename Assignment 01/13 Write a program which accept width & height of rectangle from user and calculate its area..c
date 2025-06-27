58956654de71f7a06f2836651e215b765be3572d
Atharv More <moreatharv78@gmail.com>
2025-06-27 22:11:04 +0530

    Assignment 01

---
+++ Assignment 01/13 Write a program which accept width & height of rectangle from user and calculate its area..c
@@ -0,0 +1,20 @@
+///13
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    float height,width;
+
+    printf("Enter height of Rectangle:");
+    scanf("%f",&height);
+
+    printf("Enter Width of the rectangle:");
+    scanf("%f",&width);
+
+    float area= height * width;
+
+    printf("\nArea is =%.2f\n",area);
+
+}
+
+
