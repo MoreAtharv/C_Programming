bff52402f1ec1bfaebe6d588ca7a916ad505ac52
Atharv More <moreatharv78@gmail.com>
2025-06-28 08:28:57 +0530

    Assignment 01

---
+++ Assignment 01/17 Accepts a number from the user and calculates the square of that number..c
@@ -0,0 +1,19 @@
+///17
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+
+    int Number,Square;
+
+    printf("\nEnter Any Number:");
+    scanf("%d",&Number);
+
+    Square = Number * Number;
+
+    printf("Square of %d is = %d \n",Number,Square);
+
+    return 0;
+
+}
+
