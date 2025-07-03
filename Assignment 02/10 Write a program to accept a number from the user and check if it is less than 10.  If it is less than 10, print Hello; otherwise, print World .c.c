349579458e1e3ed177fca34c142c354a3a68413e
Atharv More <moreatharv78@gmail.com>
2025-07-03 17:30:41 +0530

    Assignment 02

---
+++ Assignment 02/10 Write a program to accept a number from the user and check if it is less than 10.  If it is less than 10, print Hello; otherwise, print World .c.c
@@ -0,0 +1,23 @@
+///10
+#include<stdio.h>
+#include<conio.h>
+int main()
+
+{
+    int n;
+    printf("Enter Number:");
+    scanf("%d",&n);
+
+    if(n<10)
+    {
+        printf("\nHello");
+    }
+    else
+    {
+        printf("\nWorld");
+    }
+
+    getch();
+
+    return 0;
+}
