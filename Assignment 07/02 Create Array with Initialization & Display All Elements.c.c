192fc3d2548ce2620bc9e42a47ba1c73b307b204
Atharv More <moreatharv78@gmail.com>
2025-07-08 18:27:41 +0530

    Assignment 07

---
+++ Assignment 07/02 Create Array with Initialization & Display All Elements.c.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    int i,arr[5]={10,20,30,40,50};
+
+    printf("Elements's Are:\n");
+
+    for(i=0;i<5;i++)
+    {
+     printf("Element %d = %d\n",i+0,arr[i]);
+    }
+    return 0;
+}
