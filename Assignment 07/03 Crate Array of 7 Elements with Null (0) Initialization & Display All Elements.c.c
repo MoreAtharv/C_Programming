7cf7277733eafc130b91891d8c636297abe7ce92
Atharv More <moreatharv78@gmail.com>
2025-07-08 18:55:21 +0530

    Assignment 07

---
+++ Assignment 07/03 Crate Array of 7 Elements with Null (0) Initialization & Display All Elements.c.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    int i,arr[7]={0};
+
+    printf("Element's Are:\n");
+
+    for(i=0;i<7;i++)
+    {
+     printf("Element:%d\n",arr[i]);
+    }
+    return 0;
+}
+
