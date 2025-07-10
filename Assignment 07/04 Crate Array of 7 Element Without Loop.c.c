15f5cc83c2f294656b20c70e2f413eeeb77debf0
Atharv More <moreatharv78@gmail.com>
2025-07-10 22:14:32 +0530

    Assignment 07

---
+++ Assignment 07/04 Crate Array of 7 Element Without Loop.c.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdio.h>
+
+int main()
+{
+    int arr[7]={0};
+
+    printf("Enter 7 Element's\n");
+    scanf("%d",&arr[0]);
+    scanf("%d",&arr[1]);
+    scanf("%d",&arr[2]);
+    scanf("%d",&arr[3]);
+    scanf("%d",&arr[4]);
+    scanf("%d",&arr[5]);
+    scanf("%d",&arr[6]);
+
+    getch();
+
+    printf("Element's\n");
+
+    printf("%d\n",arr[0]);
+    printf("%d\n",arr[1]);
+    printf("%d\n",arr[2]);
+    printf("%d\n",arr[3]);
+    printf("%d\n",arr[4]);
+    printf("%d\n",arr[5]);
+    printf("%d\n",arr[6]);
+
+    return 0;
+}
