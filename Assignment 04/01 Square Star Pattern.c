7f45ff525d00d3b44cd58acae42173b23cf8bb70
Atharv More <moreatharv78@gmail.com>
2025-07-11 19:39:18 +0530

    Assignment 04

---
+++ Assignment 04/01 Square Star Pattern.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+    int i,j,Rc;
+
+    printf("Enter Number");
+    scanf("%d",&Rc);
+
+    for(i=1; i<=Rc; i++)
+    {
+        for(j=1;j<=Rc;j++)
+        {
+            printf(" * ");
+        }
+        printf("\n");
+
+    }
+    return 0;
+}
