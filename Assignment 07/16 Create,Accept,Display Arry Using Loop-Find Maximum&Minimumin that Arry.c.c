b015a049f3b236b36a98cc64a3399e58218472a4
Atharv More <moreatharv78@gmail.com>
2025-07-15 09:22:06 +0530

    Assignment 07

---
+++ Assignment 07/16 Create,Accept,Display Arry Using Loop-Find Maximum&Minimumin that Arry.c.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<conio.h>
+#define size 7
+int main()
+{
+    int i=0,Min =0,Max=0,arr[size]={};
+
+    for(i=0;i<size;i++)
+    {
+    printf("Enter Element %d:",i+1);
+    scanf("%d",&arr[i]);
+    }
+
+
+   for(i=0;i<size;i++)
+   {
+       if((i==0)||(Max < arr[i]))
+        {
+        Max = arr[i];
+        }
+
+
+        if((i==0)||(Min > arr[i]))
+        {
+        Min = arr[i];
+        }
+   }
+   printf("min=%d",Min);
+   printf("max=%d",Max);
+
+   getch();
+   return 0;
+
+}
+
